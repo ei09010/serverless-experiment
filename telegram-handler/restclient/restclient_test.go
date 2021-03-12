97dd32006ebddce23e16a4671ffb808420bb4539
Mário Jorge Pereira <jorge.pereira@farfetch.com>
2021-03-12 00:22:51 +0000

    working on restclient error cases

--- telegram-handler/restclient/restclient_test.go
+++ telegram-handler/restclient/restclient_test.go
@@ -10,6 +10,127 @@ import (
 
 func TestRestClient(t *testing.T) {
 
+	t.Run("Error post to telegram request", func(t *testing.T) {
+
+		expectedResponseText := "{\"ok\": true,\"result\": {\"message_id\": 45,\"from\": {\"id\": 1025326803,\"is_bot\": true,\"first_name\": \"MyDailyFact\",\"username\": \"majoFFper_bot\"},\"chat\": {\"id\": 690639026,\"first_name\": \"Mário\",\"type\": \"private\"},\"date\": 1615076796,\"text\": \"Product Owners never ask Chuck Norris for more features. They ask for mercy.\"}}"
+
+		// Arrange
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			if r.Method == http.MethodPost {
+				w.Write([]byte("{\"ok\": true,\"result\": {\"message_id\": 45,\"from\": {\"id\": 1025326803,\"is_bot\": true,\"first_name\": \"MyDailyFact\",\"username\": \"majoFFper_bot\"},\"chat\": {\"id\": 690639026,\"first_name\": \"Mário\",\"type\": \"private\"},\"date\": 1615076796,\"text\": \"Product Owners never ask Chuck Norris for more features. They ask for mercy.\"}}"))
+			}
+		}))
+
+		telegramClient := &BaseClient{url: ts.URL}
+
+		// Act
+		response, err := telegramClient.PostResponse(123, "stuff happened")
+
+		if err != nil {
+			t.Fatal("Everything should be ok")
+		}
+
+		// Assert
+
+		assert.EqualValues(t,
+			expectedResponseText,
+			response)
+
+	})
+
+	t.Run("Error joke request", func(t *testing.T) {
+
+		expectedId := 479
+		expectedjoke := "Chuck Norris does not need to know about class factory pattern. He can instantiate interfaces."
+		expectedType := "success"
+		expectedCategories := []string([]string{"nerdy"})
+
+		// Arrange
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			if r.Method == http.MethodGet {
+				w.Write([]byte("{\"type\": \"success\",\"value\": {\"id\": 479,\"joke\": \"Chuck Norris does not need to know about class factory pattern. He can instantiate interfaces.\",\"categories\": [\"nerdy\"]}}"))
+			}
+		}))
+
+		jokeClient := &BaseClient{url: ts.URL}
+
+		// Act
+		response, err := jokeClient.GetJoke()
+
+		if err != nil {
+			t.Fatal("Everything should be ok")
+		}
+
+		// Assert
+
+		assert.EqualValues(t,
+			expectedId,
+			response.Value.ID)
+
+		assert.EqualValues(t,
+			expectedjoke,
+			response.Value.Joke)
+
+		assert.EqualValues(t,
+			expectedType,
+			response.Type)
+
+		assert.EqualValues(t,
+			expectedCategories,
+			response.Value.Categories)
+
+	})
+
+	t.Run("Error fact request", func(t *testing.T) {
+
+		expectedId := "96221b11-8a37-4495-baf0-134be4feffc1"
+		expectedText := "To Ensure Promptness, one is expected to pay beyond the value of service – hence the later abbreviation: T.I.P."
+		expectedSourceUrl := "http://www.djtech.net/humor/useless_facts.htm"
+		expectedLanguage := "en"
+		expectedPermalink := "https://uselessfacts.jsph.pl/96221b11-8a37-4495-baf0-134be4feffc1"
+
+		// Arrange
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			if r.Method == http.MethodGet {
+				w.Write([]byte("{\"id\": \"96221b11-8a37-4495-baf0-134be4feffc1\", \"text\": \"To Ensure Promptness, one is expected to pay beyond the value of service – hence the later abbreviation: T.I.P.\", \"source\": \"djtech.net\", \"source_url\": \"http://www.djtech.net/humor/useless_facts.htm\", \"language\": \"en\", \"permalink\": \"https://uselessfacts.jsph.pl/96221b11-8a37-4495-baf0-134be4feffc1\"}"))
+			}
+		}))
+
+		factClient := &BaseClient{url: ts.URL}
+
+		// Act
+		response, err := factClient.GetFact()
+		if err != nil {
+			t.Fatal("Everything should be ok")
+		}
+
+		// Assert
+
+		assert.EqualValues(t,
+			expectedText,
+			response.Text)
+
+		assert.EqualValues(t,
+			expectedId,
+			response.ID)
+
+		assert.EqualValues(t,
+			expectedSourceUrl,
+			response.SourceURL)
+
+		assert.EqualValues(t,
+			expectedLanguage,
+			response.Language)
+
+		assert.EqualValues(t,
+			expectedPermalink,
+			response.Permalink)
+
+	})
+
 	t.Run("Successful post to telegram request", func(t *testing.T) {
 
 		expectedResponseText := "{\"ok\": true,\"result\": {\"message_id\": 45,\"from\": {\"id\": 1025326803,\"is_bot\": true,\"first_name\": \"MyDailyFact\",\"username\": \"majoFFper_bot\"},\"chat\": {\"id\": 690639026,\"first_name\": \"Mário\",\"type\": \"private\"},\"date\": 1615076796,\"text\": \"Product Owners never ask Chuck Norris for more features. They ask for mercy.\"}}"
