039291f8c2addce922c37da969b35afa40d8976f
Mário Jorge Pereira <jorge.pereira@farfetch.com>
2021-03-15 21:47:34 +0000

    minor refactoring, every unit test in handler is working

--- telegram-handler/main_test.go
+++ telegram-handler/main_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHandler(t *testing.T) {
+func TestHandlerFailedPostTelegramRequest(t *testing.T) {
 
-	t.Run("Failed Post Response", func(t *testing.T) {
+	t.Run("Failed Post Telegram Request", func(t *testing.T) {
 
 		expectedTelegramResponse := "issue in telegram"
 
@@ -75,57 +75,9 @@ func TestHandler(t *testing.T) {
 			expectedTelegramResponse,
 			response.Body)
 	})
+}
 
-	t.Run("Failed Fact Request", func(t *testing.T) {
-
-		mocks.ReturnGetFact = func() (*restclient.GeneratedFact, error) {
-
-			return nil, ErrNon200Response
-		}
-
-		telegramRequest := Update{
-			Message: Message{
-				Text: "/fact",
-				Chat: Chat{
-					Id: 1234,
-				},
-			},
-			UpdateId: 1,
-		}
-
-		requestBody, err := json.Marshal(telegramRequest)
-
-		if err != nil {
-			t.Fatal("Can't run test scenario")
-		}
-
-		tempRequest := events.APIGatewayProxyRequest{
-			Body:       string(requestBody),
-			Path:       "http://myTelegramWebHookHandler.com/secretToken",
-			HTTPMethod: "POST",
-		}
-
-		myMockClient := &mocks.MockBaseClient{}
-
-		restclient.MyFactClient = myMockClient
-
-		restclient.MyTelegramClient = myMockClient
-
-		// Act
-		response, err := handler(tempRequest)
-
-		// Assert
-
-		assert.Equal(t, 0, myMockClient.ReturnGetJokeCallCount)
-
-		assert.Equal(t, 1, myMockClient.ReturnGetFactCallCount)
-
-		assert.Equal(t, 0, myMockClient.ReturnPostResponseCallCount)
-
-		assert.EqualValues(t,
-			"Error calling fact generation api",
-			response.Body)
-	})
+func TestHandlerFailedJokeRequest(t *testing.T) {
 
 	t.Run("Failed Joke Request", func(t *testing.T) {
 
@@ -177,6 +129,9 @@ func TestHandler(t *testing.T) {
 			"Error calling joke generation api",
 			response.Body)
 	})
+}
+
+func TestHandlerSuccessfulJokeRequest(t *testing.T) {
 
 	t.Run("Successful Joke Request", func(t *testing.T) {
 
@@ -242,6 +197,62 @@ func TestHandler(t *testing.T) {
 			`{"ok": true,"result": {"message_id": 26,"from": {"id": 1025326803,"is_bot": true,"first_name": "MyDailyFact","username": "majoFFper_bot"},"chat": {"id": -255361673,"title": "Pokémons","type": "group","all_members_are_administrators": true},"date": 1614894279,"text": "To Ensure Promptness, one is expected to pay beyond the value of service – hence the later abbreviation: T.I.P."}}`,
 			response.Body)
 	})
+}
+
+func TestHandlerFailedFactRequest(t *testing.T) {
+	t.Run("Failed Fact Request", func(t *testing.T) {
+
+		mocks.ReturnGetFact = func() (*restclient.GeneratedFact, error) {
+
+			return nil, ErrNon200Response
+		}
+
+		telegramRequest := Update{
+			Message: Message{
+				Text: "/fact",
+				Chat: Chat{
+					Id: 1234,
+				},
+			},
+			UpdateId: 1,
+		}
+
+		requestBody, err := json.Marshal(telegramRequest)
+
+		if err != nil {
+			t.Fatal("Can't run test scenario")
+		}
+
+		tempRequest := events.APIGatewayProxyRequest{
+			Body:       string(requestBody),
+			Path:       "http://myTelegramWebHookHandler.com/secretToken",
+			HTTPMethod: "POST",
+		}
+
+		myMockClient := &mocks.MockBaseClient{}
+
+		restclient.MyFactClient = myMockClient
+
+		restclient.MyTelegramClient = myMockClient
+
+		// Act
+		response, err := handler(tempRequest)
+
+		// Assert
+
+		assert.Equal(t, 0, myMockClient.ReturnGetJokeCallCount)
+
+		assert.Equal(t, 1, myMockClient.ReturnGetFactCallCount)
+
+		assert.Equal(t, 0, myMockClient.ReturnPostResponseCallCount)
+
+		assert.EqualValues(t,
+			"Error calling fact generation api",
+			response.Body)
+	})
+}
+
+func TestHandlerSuccessfulFactRequest(t *testing.T) {
 
 	t.Run("Successful Fact Request", func(t *testing.T) {
 
@@ -255,6 +266,13 @@ func TestHandler(t *testing.T) {
 				Language:  "en",
 				Permalink: "teste124",
 			}, nil
+		}
+
+		mocks.ReturnPostResponse = func(chatId int, text string) (string, error) {
+
+			escapedJsonContent := "{\"ok\": true,\"result\": {\"message_id\": 26,\"from\": {\"id\": 1025326803,\"is_bot\": true,\"first_name\": \"MyDailyFact\",\"username\": \"majoFFper_bot\"},\"chat\": {\"id\": -255361673,\"title\": \"Pokémons\",\"type\": \"group\",\"all_members_are_administrators\": true},\"date\": 1614894279,\"text\": \"To Ensure Promptness, one is expected to pay beyond the value of service – hence the later abbreviation: T.I.P.\"}}"
+
+			return escapedJsonContent, nil
 		}
 
 		telegramRequest := Update{
